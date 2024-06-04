b1d556257ea8734bb2e9f387b039324633bd4306
Henri <henrimerilainen@kamk.fi>
2024-06-04 10:28:48 +0300

    Tehtävä 3.3 - puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const app = express()
 
 app.use(express.json())
 
-persons = [
+let persons = [
   {
-    "id": "1",
+    "id": 1,
     "name": "Arto Hellas",
     "number": "040-123456",
   },
   {
-    "id": "2",
+    "id": 2,
     "name": "Ada Lovelace",
     "number": "39-44-5323523",
   },
   {
-    "id": "3",
+    "id": 3,
     "name": "Dan Abramov",
     "number": "12-43-234345",
   },
   {
-    "id": "4",
+    "id": 4,
     "name": "Mary Poppendick",
     "number": "39-23-6423122"
   }
@@ -35,9 +35,20 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const time = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${time}</p>`)
+  const time = new Date()
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p>${time}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
