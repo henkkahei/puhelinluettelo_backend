1f36729f47f0b63b5e8a6e10e7c301dae562a090
Henri <henrimerilainen@kamk.fi>
2024-06-05 11:09:53 +0300

    Tehtävä 3.9 - puhelinluettelon backend step9 (backend-osuus)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
@@ -64,7 +67,12 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
   persons = persons.filter(person => person.id !== id)
+
+  console.log(person)
+
+  response.json(person)
 
   response.status(204).end()
 })
@@ -78,7 +86,9 @@ const generateRandomId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name | body.number) {
+  console.log(body)
+
+  if (!body.name | !body.number) {
     return response.status(400).json({
         error: 'include both name and number'
     })
