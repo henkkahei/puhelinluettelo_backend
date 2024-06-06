bcb6942a2947948ad8831afc2b1614ab7b51951d
Henri <henrimerilainen@kamk.fi>
2024-06-06 12:58:13 +0300

    Tehtävä 3.19 - puhelinluettelo ja tietokanta step7

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.delete('/api/persons/:id', (request, response) => {
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   console.log(body)
@@ -79,6 +79,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -101,6 +102,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
   }
 
   next(error)
