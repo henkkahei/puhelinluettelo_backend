7737da0b16ed5d24e81533d232b817bfb079fc85
Henri <henrimerilainen@kamk.fi>
2024-06-06 11:35:11 +0300

    Tehtävä 3.17 & 3.18 - puhelinluettelo ja tietokanta step5 & step6

--- index.js
+++ index.js
@@ -22,29 +22,6 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-// let persons = [
-//   {
-//     "id": 1,
-//     "name": "Arto Hellas",
-//     "number": "040-123456",
-//   },
-//   {
-//     "id": 2,
-//     "name": "Ada Lovelace",
-//     "number": "39-44-5323523",
-//   },
-//   {
-//     "id": 3,
-//     "name": "Dan Abramov",
-//     "number": "12-43-234345",
-//   },
-//   {
-//     "id": 4,
-//     "name": "Mary Poppendick",
-//     "number": "39-23-6423122"
-//   }
-// ]
-
 app.get('/', (request, response) => {
   response.send('<h1>Puhelinluettelo</h1>')
 })
@@ -77,9 +54,9 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
-      response.json(result)
+      response.json(result).end()
       console.log(result)
-      response.status(204).end()
+      // response.status(204).end()
     })
 })
 
@@ -94,12 +71,6 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  // if (persons.some((item) => item.name === body.name)) {
-  //   return response.status(400).json({
-  //       error: 'name must be unique'
-  //   })
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number
@@ -108,6 +79,21 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
